7868d776ac8b75ba2dfc63c7139f214913a48392
Madboy0700 <bobimobi07@gmail.com>
2024-08-18 10:53:58 +0300

    Update seek.py

--- AnonXMusic/plugins/admins/seek.py
+++ AnonXMusic/plugins/admins/seek.py
@@ -10,7 +10,7 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command(["seek", "cseek", "seekback", "cseekback"])
+    filters.command(["ilerisar", "cilerisar", "gerisar", "cgerisar"])
     & filters.group
     & ~BANNED_USERS
 )
