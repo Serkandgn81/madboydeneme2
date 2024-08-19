8f97e6becd12f1aedf1ef4f8d606c05f395d1bed
Serkandgn81 <doganserkan344@gmail.com>
2024-08-19 20:47:29 +0300

    Update play.py

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -29,6 +29,7 @@ from config import BANNED_USERS, lyrical
     filters.command(
         [
             "play",
+            "oynat",
             "voynat",
             "cplay",
             "cvoynat",
