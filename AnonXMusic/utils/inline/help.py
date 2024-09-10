f2fbabee350513fe2fc4f4b1ce3e4b78827189fb
Serkandgn81 <doganserkan344@gmail.com>
2024-09-10 09:58:32 +0300

    Update help.py

--- AnonXMusic/utils/inline/help.py
+++ AnonXMusic/utils/inline/help.py
@@ -24,7 +24,7 @@ def help_pannel(_, START: Union[bool, int] = None):
                 
                 InlineKeyboardButton(
                     text=_["S_B_11"],
-                    callback_data="help_callback hb1",
+                    callback_data="help_callback hb11",
                 ),
                 
                 InlineKeyboardButton(
